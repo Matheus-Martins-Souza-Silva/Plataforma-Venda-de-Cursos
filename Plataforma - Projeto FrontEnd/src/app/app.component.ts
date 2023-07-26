bea4a7ce1dc53545a8f7d1100b6f1e2f81d1f157
Matheus Martins <martinsmatheus066@gmail.com>
2023-07-26 02:12:13 -0300

    Adding BootStrap And Fixing Connection

--- Plataforma - Projeto FrontEnd/src/app/app.component.ts
+++ Plataforma - Projeto FrontEnd/src/app/app.component.ts
@@ -1,17 +1,25 @@
 import { Component } from '@angular/core';
 import { TesteService } from './teste.service';
+import { setTheme } from 'ngx-bootstrap/utils';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'Plataforma';
 
-  constructor(private testeService: TesteService) {}
+  constructor(private testeService: TesteService) {
+    setTheme('bs5'); // or 'bs4'
+  }
 
   OnTest() {
     this.testeService.OnSendTest();
+  }
+
+  DestroyIt() {
+    
   }
 }
--- Plataforma - Projeto FrontEnd/src/app/app.module.ts
+++ Plataforma - Projeto FrontEnd/src/app/app.module.ts
@@ -4,17 +4,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { ProgressbarConfig } from 'ngx-bootstrap/progressbar';
+import { ProgressbarModule } from 'ngx-bootstrap/progressbar';
+import { TestBootstrapComponent } from './TesteBootStrap/test-bootstrap/test-bootstrap.component';
+
+export function getProgressbarConfig(): ProgressbarConfig {
+  return Object.assign(new ProgressbarConfig(), { animate: true, striped: true,  max: 150 });
+}
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestBootstrapComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    TooltipModule.forRoot(),
+    BsDropdownModule.forRoot(),
+    ProgressbarModule.forRoot()
   ],
-  providers: [],
+  providers: [{ provide: ProgressbarConfig, useFactory: getProgressbarConfig }],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+ }
